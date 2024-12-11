2e8527450a8ce703d3bf3d79fd1fc8464b9e1ea4
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2024-12-11 16:21:15 -0600

    Add Login

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,21 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const User = require("@models/User");
 
 export const authRouter = express.Router();
 
-authRouter.post("/register", async (req: any, res: any) => {
+authRouter.post("/register", async (req: Request, res: any) => {
   try {
-    console.log({
-      Body: req.body,
-    });
     const { username, email, password } = req.body;
     const newUser = new User({
       username,
       email,
       password,
     });
-
-    // Check if the email already exists
     const existingEmail = await User.findOne({
       email,
     });
@@ -23,7 +18,6 @@ authRouter.post("/register", async (req: any, res: any) => {
       return res.status(400).json("Email already exists");
     }
 
-    // Check if the username already exists
     const existingUsername = await User.findOne({
       username: req.body.username,
     });
@@ -31,9 +25,28 @@ authRouter.post("/register", async (req: any, res: any) => {
       return res.status(400).json("Username already exists");
     }
 
-    // Save the user and respond with the user
     const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
+authRouter.post("/login", async (req: Request, res: any) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      return res.status(400).json("Invalid email");
+    }
+    if (user.password !== password) {
+      return res.status(400).json("Invalid password");
+    }
+
+    console.log({ "User found": user });
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
